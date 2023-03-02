82a5e0b3a81984fbe52881079e06ccabe51d1496
RitvarsZuns <ritvars.zuns@edu.rtu.lv>
2023-03-02 21:13:39 +0200

    check

--- tree_height.py
+++ tree_height.py
@@ -7,12 +7,50 @@ import numpy
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    heights = [0] * n
+
+    for i in range(n):
+        node_height = 0
+        current = i
+        while current != -1:
+            if heights[current] != 0:
+                node_height += heights[current]
+                break
+            node_height += 1
+            current = parents[current]
+        heights[i] = node_height
+    return max(heights)
 
 
 def main():
+    while True:
+        mode = input("Enter input mode (I for console input, F for file input): ")
+        if mode.lower() == "i":
+            n = int(input("Enter number of nodes: "))
+            parents = list(map(int, input("Enter parents of nodes: ").split()))
+            break
+        elif mode.lower() == "f":
+            while True:
+                try:
+                    filename = input("Enter file name (without 'a' in the name): ")
+                    if 'a' in filename:
+                        raise ValueError("File name cannot contain 'a'")
+                    with open(f"./{filename}", "r") as f:
+                        n = int(f.readline().strip())
+                        parents = list(map(int, f.readline().strip().split()))
+                    break
+                except FileNotFoundError:
+                    print("File not found. Please try again.")
+                except ValueError as e:
+                    print(str(e))
+            break
+        else:
+            print("Invalid input mode. Please enter I or F.")
+
+    print(compute_height(n, parents))
+            
+
+
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -21,7 +59,7 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+    
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
@@ -29,5 +67,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+#main()
+#print(numpy.array([1,2,3]))
