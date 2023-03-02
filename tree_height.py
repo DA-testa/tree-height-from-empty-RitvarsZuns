e88c9b56b817d512a8a659e37f01ba33aebd5c0b
RitvarsZuns <ritvars.zuns@edu.rtu.lv>
2023-03-02 22:25:10 +0200

    check

--- tree_height.py
+++ tree_height.py
@@ -24,27 +24,23 @@ def compute_height(n, parents):
 
 def main():
     mode = input()
-    while True:
-        
-        if mode.lower() == "i":
-            n = int(input())
-            parents = list(map(int, input().split()))
-            break
-        elif mode.lower() == "f":
-            while True:
-                try:
-                    filename = input("Enter file name (without 'a' in the name): ")
-                    if 'a' in filename:
-                        raise ValueError("File name cannot contain 'a'")
-                    with open(f"./{filename}", "r") as f:
-                        n = int(f.readline().strip())
-                        parents = list(map(int, f.readline().strip().split()))
-                    break
-                except FileNotFoundError:
-                    print("File not found. Please try again.")
-                except ValueError as e:
-                    print(str(e))
-            break
+    if mode.lower() == "i":
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif mode.lower() == "f":
+        while True:
+            try:
+                filename = input("Enter file name (without 'a' in the name): ")
+                if 'a' in filename:
+                    raise ValueError("File name cannot contain 'a'")
+                with open(f"./{filename}", "r") as f:
+                    n = int(f.readline().strip())
+                    parents = list(map(int, f.readline().strip().split()))
+                break
+            except FileNotFoundError:
+                print("File not found. Please try again.")
+            except ValueError as e:
+                print(str(e))
         #else:
             #print("Invalid input mode. Please enter I or F.")
 
